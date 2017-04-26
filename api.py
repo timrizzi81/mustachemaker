0235e20553b88e9d5cc701e23f0b831107d8dd84
Michael Polino <mrpolino@gmail.com>
2017-04-25 18:07:11 -0700

    adds test image and xml model files

--- api.py
+++ api.py
@@ -5,23 +5,6 @@ from boto.s3.connection import S3Connection
 from boto.s3.key import Key
 import os
 
-def pull_file_from_s3(key):
-    def get_bucket():
-	    access= os.environ['SECRET_ENV_AWS_ACCESS_KEY_SHARED']
-	    secret= os.environ['SECRET_ENV_AWS_SECRET_KEY_SHARED']
-	    conn = S3Connection(access,secret)
-	    b = conn.get_bucket('ds-cloud-demonstration-shared',validate=False)
-	    return b
-
-    s3_bucket = get_bucket()
-    payload = s3_bucket.get_key(key)
-    local_file = payload.get_contents_to_filename(key)
-    return True
-
-if not os.path.isfile('haarcascade_frontalface_default.xml'):
-    pull_file_from_s3('haarcascade_frontalface_default.xml')
-    pull_file_from_s3('haarcascade_mcs_nose.xml')
-
 import mustache
 
 def run(img):
